88a30cc80d2b114d7df5a1a9e4f5857186f88086
Nglpr Tech <nglpr.tech@gmail.com>
2023-02-14 15:56:51 +0700

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -1,3 +1,28 @@
+# Master Nodes
+for host in master{0..1}.k8s.nglp.tech; do
+  ssh-copy-id root@$host
+done
+
+# Worker Nodes
+for host in worker{0..1}.k8s.nglp.tech; do
+  ssh-copy-id root@$host
+done
+
+
+kubeadm init \
+  --pod-network-cidr=192.168.0.0/16 \
+  --control-plane-endpoint=endpoint.k8s.nglp.tech \
+  --cri-socket=/var/run/crio/crio.sock \
+  --upload-certs
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+mkdir -p $HOME/.kube
+scp -r user@remote.host.domain:/path/user/.kube .
+cp -r .kube $HOME/
+
 helm upgrade --install gitlab gitlab/gitlab -n gitlab-system\
   --timeout 600s \
   --set global.hosts.domain=k8s.nglp.tech \
@@ -9,7 +34,7 @@ helm upgrade --install gitlab gitlab/gitlab -n gitlab-system\
 
 helm install -n nfs-provisioner nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     --set nfs.server=10.0.150.91 \
-    --set nfs.path=/nfs \
+    --set nfs.path=/nfs/vanilla \
     --set storageClass.onDelete=delete \
     --set storageClass.pathPattern='${.PVC.namespace}/${.PVC.name}' \ #'${.PVC.namespace}/${.PVC.name}'
     --set storageClass.provisionerName=storage.io/nfs \
